368e5d8a0966b35d29a6634672ca48a42b6634bf
tntran09 <ttkbnt@hotmail.com>
2016-02-03 21:01:09 -0600

    Deleted artifacts, files, tests from original package

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Compound = require('./lib/compound');
-
-module.exports = function (formula) {
-    // considering adding optional parameter to list to return the compound rather than just the mass
-    var c = new Compound(formula);
-    return c.molarMass;
-};
