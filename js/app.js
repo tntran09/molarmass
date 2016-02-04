56b317189b608382c14b6502eda875b773a948a1
tntran09 <ttkbnt@hotmail.com>
2016-02-03 21:44:48 -0600

    Added base working content for GitHub Pages site
    - Used watchify to build js/bundle.js
    - Used electron to deploy/demo locally
    - Used ReactJS to build the frontend app
    - Created all React components in app.js
    - Used molarmass package to calculate mass of active formula live
    - Created top-level components for InputSection, ActiveSection, and
    HistorySection
    - Used <sub> to output chemical formulas with subscripts

---
+++ js/app.js
@@ -0,0 +1,100 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var molarmass = require('molarmass');
+
+//MolarMassApp.React.js
+var MolarMassApp = React.createClass({
+  getInitialState: function () {
+    return {
+      formula: '',
+      history: []
+    }
+  },
+
+  _handleChange: function (formula) {
+    this.setState({
+      formula: formula
+    });
+  },
+
+  render: function () {
+    return (
+      <div id="molarMassApp">
+        <InputSection formula={this.state.formula} handleChange={this._handleChange} />
+        <ResultsSection formula={this.state.formula} />
+      </div>
+    );
+  }
+});
+
+//InputSection.React.js
+var InputSection = React.createClass({
+  _onChange: function (event) {
+    this.props.handleChange(this.refs.formulaInput.value);
+  },
+
+  render: function () {
+    return (
+      <div id="inputSection">
+        <form>
+          <input type="text" placeholder="Examples: H20, (NH4)2Cr2O7" ref="formulaInput" onChange={this._onChange}/>
+          <button type="submit">-&gt;</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+//ResultsSection.React.js
+var ResultsSection = React.createClass({
+  render: function () {
+    return (
+      <div id="resultsSection">
+        <ActiveCompoundSection formula={this.props.formula}/>
+        <HistorySection />
+      </div>
+    );
+  }
+});
+
+//ActiveCompoundSection.React.js
+var ActiveCompoundSection = React.createClass({
+  render: function () {
+    var mass = 0.0;
+    var formulaAsHTML = {
+      __html: this.props.formula.replace(/[0-9]+/g, '<sub>$&</sub>')
+    };
+
+    try {
+      mass = molarmass(this.props.formula);
+    }
+    catch (e) {
+
+    }
+
+    return (
+      <div id="activeCompoundSection">
+        <h1 className="chemicalText" dangerouslySetInnerHTML={formulaAsHTML}></h1>
+        <p>Molar Mass: {mass}</p>
+
+
+        <table></table>
+      </div>
+    );
+  }
+});
+
+//
+var HistorySection = React.createClass({
+  render: function () {
+    return (
+      <table>
+      </table>
+    );
+  }
+});
+
+ReactDOM.render(
+  <MolarMassApp />,
+  document.getElementById('appContainer')
+);
